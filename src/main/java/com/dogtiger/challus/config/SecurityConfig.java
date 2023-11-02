cd1f618d31260068aaaf982e7f179f227343f25c
younghk37 <younghk37@gmail.com>
2023-11-02 18:32:32 +0900

    Feat: SecurityConfig 추가, WebMvcConfig 추가-#1

---
+++ src/main/java/com/dogtiger/challus/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.dogtiger.challus.config;
+
+import com.dogtiger.challus.security.oauth2.OAuth2SuccessHandler;
+import com.dogtiger.challus.service.PrincipalUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PrincipalUserDetailsService principalUserDetailsService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        http.csrf().disable();
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .oauth2Login()
+                .loginPage("http://localhost:3000/auth/signin")
+                .successHandler(oAuth2SuccessHandler)
+                .userInfoEndpoint()
+                .userService(principalUserDetailsService);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
