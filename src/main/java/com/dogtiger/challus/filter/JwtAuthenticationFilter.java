2a45f0a6c29b735a2b2dab16c46bc00f34c6aa9f
younghk37 <younghk37@gmail.com>
2023-11-05 15:19:29 +0900

    Feat: JwtAuthenticationFilter 추가-#29

---
+++ src/main/java/com/dogtiger/challus/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,32 @@
+package com.dogtiger.challus.filter;
+
+import com.dogtiger.challus.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        String bearerToken = httpServletRequest.getHeader("Authorization");
+        String token = jwtProvider.removeBearer(bearerToken);
+        Authentication authentication = jwtProvider.getAuthentication(token);
+
+        if(authentication != null) {
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
+    }
+}
