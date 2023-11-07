d9cc4ada0b38ea0e486241e2eef2814e9f323791
geunhaemoon <nevve1@duam.net>
2023-11-07 15:18:23 +0900

    Fix: 글 목록 불러오기 수정중

--- src/main/java/com/dogtiger/challus/controller/NoticeController.java
+++ src/main/java/com/dogtiger/challus/controller/NoticeController.java
@@ -1,13 +1,17 @@
 package com.dogtiger.challus.controller;
 
 
-import com.dogtiger.challus.dto.NoticewriteReqDto;
+import com.dogtiger.challus.dto.NoticeListRespDto;
+import com.dogtiger.challus.dto.NoticeWriteReqDto;
 import com.dogtiger.challus.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +19,12 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @PostMapping("/api/notice")
-    public ResponseEntity<?> noticewrite(@RequestBody NoticewriteReqDto noticewriteReqDto){
-
+    public ResponseEntity<?> noticeWrite(@RequestBody NoticeWriteReqDto noticewriteReqDto){
         return ResponseEntity.ok(noticeService.saveNotice(noticewriteReqDto));
+    }
+    @GetMapping("/api/notices")
+    public ResponseEntity<?> noticeListGet(){
+        List<NoticeListRespDto> noticeList = noticeService.noticeListGet();
+        return ResponseEntity.ok(noticeList);
     }
 }
--- src/main/java/com/dogtiger/challus/service/NoticeService.java
+++ src/main/java/com/dogtiger/challus/service/NoticeService.java
@@ -1,6 +1,7 @@
 package com.dogtiger.challus.service;
 
-import com.dogtiger.challus.dto.NoticewriteReqDto;
+import com.dogtiger.challus.dto.NoticeListRespDto;
+import com.dogtiger.challus.dto.NoticeWriteReqDto;
 import com.dogtiger.challus.entity.Notice;
 import com.dogtiger.challus.entity.User;
 import com.dogtiger.challus.repository.NoticeMapper;
@@ -9,6 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class NoticeService {
@@ -16,10 +20,17 @@ public class NoticeService {
     private final UserMapper userMapper;
 
 
-    public boolean saveNotice(NoticewriteReqDto noticewriteReqDto) {
+    public boolean saveNotice(NoticeWriteReqDto noticewriteReqDto) {
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = userMapper.findUserByEmail(email);
         Notice notice = noticewriteReqDto.toNoticeEntity(user.getUserId());
         return noticeMapper.saveNotice(notice) > 0;
+    }
+
+    public List<NoticeListRespDto> noticeListGet() {
+        List<NoticeListRespDto> noticeListRespDtos = new ArrayList<>();
+        List<Notice> notices = noticeMapper.noticeList();
+
+        return NoticeListRespDto;
     }
 }
