3d2c26ea1b52c53a1fb05718568808a4ac4eaa23
younghk37 <younghk37@gmail.com>
2023-11-09 10:18:51 +0900

    Feat: 알림 리스트 조회 기능 구현-#80

---
+++ src/main/java/com/dogtiger/challus/dto/LettersResDto.java
@@ -0,0 +1,21 @@
+package com.dogtiger.challus.dto;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+public class LettersResDto {
+    private int letterId;
+    private int senderUserId;
+    private int receiverUserId;
+    private String title;
+    private String content;
+    private LocalDateTime sendDateTime;
+    private int isRead;
+    private int letterType;
+    private String targetUrl;
+}
---
+++ src/main/java/com/dogtiger/challus/repository/LetterMapper.java
@@ -0,0 +1,12 @@
+package com.dogtiger.challus.repository;
+
+import com.dogtiger.challus.entity.Letter;
+import com.dogtiger.challus.entity.Order;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface LetterMapper {
+    public List<Letter> findLettersByUserId(int userId);
+}
