861761fe95ee86213f8377fb1c4d298e51944e90
gzero1016 <gzero1016@gmail.com>
2023-11-21 01:45:40 +0900

    Feat:코드수정 및 stamp추가

---
+++ src/main/java/com/dogtiger/challus/service/StampService.java
@@ -0,0 +1,38 @@
+package com.dogtiger.challus.service;
+
+import com.dogtiger.challus.dto.AttendanceReqDto;
+import com.dogtiger.challus.entity.Challenge;
+import com.dogtiger.challus.entity.Stamp;
+import com.dogtiger.challus.repository.StampMapper;
+import com.dogtiger.challus.security.PrincipalUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class StampService {
+
+    private final StampMapper stampMapper;
+
+    public boolean saveAttendance(AttendanceReqDto attendanceReqDto) {
+        PrincipalUser principalUser = (PrincipalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        int userId = principalUser.getUser().getUserId();
+        attendanceReqDto.setUserId(userId);
+
+        return stampMapper.saveAttendance(attendanceReqDto.toStamp()) > 0;
+    }
+
+    public List<AttendanceReqDto> getAttendances() {
+        PrincipalUser principalUser = (PrincipalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        int userId = principalUser.getUser().getUserId();
+        List<Stamp> stamps = stampMapper.getAttendances(userId);
+
+        System.out.println(stamps);
+        return stamps.stream().map(Stamp::toAttendanceDto).collect(Collectors.toList());
+    }
+}
