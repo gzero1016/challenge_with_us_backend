396caac441878687b05401b7c8dbd358ea2711f1
young37 <younghk37@gmail.com>
2023-12-03 17:59:45 +0900

    Feat: 기간 지난 챌린지 속성 값 변경 시키기-#255

--- src/main/java/com/dogtiger/challus/ChallusApplication.java
+++ src/main/java/com/dogtiger/challus/ChallusApplication.java
@@ -2,8 +2,10 @@ package com.dogtiger.challus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ChallusApplication {
 
 	public static void main(String[] args) {
